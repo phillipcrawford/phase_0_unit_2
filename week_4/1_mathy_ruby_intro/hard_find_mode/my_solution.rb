4803f9c48d3f3f9196825346eb8e1df3c303af9b
Phillip P. Crawford <phillip.crawford3@gmail.com>
2014-04-22 23:42:38 -0700

    Week 4 Challenge 1

--- week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
+++ week_4/1_mathy_ruby_intro/hard_find_mode/my_solution.rb
@@ -4,22 +4,120 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [with: Kirstin Jarchow]
 
 # 1. Pseudocode
 
 # What is the input?
+
+#an array
+
 # What is the output? (i.e. What should the code return?)
+
+#an array containing only the most common values (the mode) of the inputted array
+
 # What are the steps needed to solve the problem?
+
+#set popular to one
+#make empty hash
+#make empty array (mode)
+
+#for each value in our array
+#check if value is already a key in hash 
+#if it is, increment value of key by 1
+#    check to see if value greater than popular 
+#    if it is set popular to that value
+#if it isn't create it as key in hash and set to 1
+
+#for each key in our hash
+#check to see if the value of key is equal to popular
+#if it is push it to our mode array
+    
+#return the mode array
 
 
 # 2. Initial Solution
 
-
+#def mode(array)
+#    hash = Hash.new
+#    mode = Array.new
+#    popular = 1
+    
+#    array.each do { |x|
+#        if hash.has_key?(x)
+#            hash[x] += 1
+#            if hash[x] > popular
+#                popular = hash[x]
+#            end
+#        else 
+#            hash[x] = 1
+#        end
+#    }
+    
+#    hash.each do{ |x, y|
+#        if y == popular
+#            mode.push(x)
+#        end
+#    }
+    
+#    mode
+#end
 
 
 # 3. Refactored Solution
 
-
+def mode(array)
+    hash = Hash.new
+    mode = Array.new
+    popular = 1
+    
+    array.each { |x|
+        if hash.has_key?(x)
+            hash[x] += 1
+            if hash[x] > popular
+                popular = hash[x]
+            end
+        else 
+            hash[x] = 1
+        end
+    } 
+    
+    hash.each { |x, y|
+        if y == popular
+            mode.push(x)
+        end
+    } 
+    
+    mode
+end 
 
 # 4. Reflection 
+# What parts of your strategy worked? What problems did you face?
+
+# Kirstin and I traded off between navigator and driver well. We worked together on what little syntax
+# either of us didn't know, but I think both of us coming into this prepared helped a lot. 
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+
+# Syntax questions, we used each other to answer them.
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+
+# No concepts, I get math behind this
+
+# Did you learn any new skills or tricks?
+
+# A bit of a refresher on hashes. I thought each key had to be initialized to a value or else 
+# writing into it might think values were keys. But no.
+
+# How confident are you with each of the learning objectives?
+
+# Very, I think this is harder than the hardest challenge this week.
+
+# Which parts of the challenge did you enjoy?
+
+# The math.
+
+# Which parts of the challenge did you find tedious?
+
+# I enjoyed this challenge, Kirstin & I got through it quickly
--- week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
+++ week_4/1_mathy_ruby_intro/harder_calculate_median/my_solution.rb
@@ -4,22 +4,82 @@
 # include it in this file. Also make sure everything that isn't code
 # is commented in the file.  
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 1. Pseudocode
 
 # What is the input?
+
+# an array, maybe numbers, maybe letters, they might be unsorted
+
 # What is the output? (i.e. What should the code return?)
+
+# an array containing the median if the starting array was sorted
+
 # What are the steps needed to solve the problem?
+
+# make function that takes an array as parameter
+# sort array that came as parameter
+# check to see if length of array is even or odd
+# if odd return middle value
+# if even return average of two middle values
 
 
 # 2. Initial Solution
 
+# def median(n)
+#  temp = n.length
+#  n= n.sort!
+#  if (temp % 2 == 1)
+#  	return n[(temp/2).floor]
+#  else
+#    return (n[((temp/2)-1)] + n[(temp/2)])/2
+#  end
+#end
 
 
 
 # 3. Refactored Solution
-
+def median(n)  
+  temp = n.length
+  n.sort!
+  if (temp % 2 == 1)
+    return n[(temp/2).floor]
+  else
+    return (n[temp/2-1] + n[temp/2])/2.0
+  end
+end
 
 
 # 4. Reflection 
+# What parts of your strategy worked? What problems did you face?
+
+# This problem was part of what we worked on in a group last week. I did the pseudocode for that,
+# so I had an advantage in just remembering how I did median in javascript and doing something similar in 
+# ruby. The hurdles I faced were realizing the array needed to be sorted, and that my division calculation
+# for even length arrays was dividing integers instead of decimals.
+
+# What questions did you have while coding? What resources did you find to help you answer them?
+
+# No questions really, beyond looking at rspec failures and thinking what is the hurdle I'm currently 
+# facing? Time got me the answer.
+
+# What concepts are you having trouble with, or did you just figure something out? If so, what?
+
+# No concepts 
+
+# Did you learn any new skills or tricks?
+
+# Practiced ruby syntax.
+
+# How confident are you with each of the learning objectives?
+
+# I'm confident about building a median function in ruby
+
+# Which parts of the challenge did you enjoy?
+
+# The mathyness of it
+
+# Which parts of the challenge did you find tedious?
+
+# Debugging
